a66c6eb94dc6557cf99c309a7034758cb47a058e
antoinebayssac <antoine.bayssac@edu.devinci.fr>
2022-11-28 09:24:36 +0100

    :hammer: add model cour

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,61 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+
+
+Route::get('/tasks', function ()  {
+    return \App\Models\Task::all();
+});
+
+Route::get('/tasks/{id}', function ($id) {
+    $task = \App\Models\Task::find($id);
+
+    if ($task) {
+        return $task;
+    }
+});
+
+
+
+Route::put('/tasks/{taskId}', function ($taskId) {
+
+   $task = \App\Models\Task::find($taskId);
+   $data = request()->only(['title', 'content', 'done', 'priority']);
+
+   request()->validate([
+      'title' => 'required',
+      'content' => 'required',
+      'done' => 'required',
+      'priority' => 'required'
+   ]);
+
+    $task->update($data);
+
+});
+
+Route::post('/tasks', function () {
+    request()->validate([
+       'title' => 'required'
+    ]);
+
+    $data = request()->all();
+
+    return \App\Models\Task::create($data);
+});
+
+Route::delete('/tasks/{taskId}', function ($taskId) {
+
+    $task = \App\Models\Task::find($taskId);
+
+    if(!$task) {
+        return response("Not found", 404);
+    }
+
+    $task->delete();
+
 });
