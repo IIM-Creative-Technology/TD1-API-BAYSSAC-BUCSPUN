8b5f6696781302dd233f204a33253d382be9987b
Denis Bucspun <bucspun.d@gmail.com>
2022-11-28 09:45:24 +0100

    :card_file_box: add migration

---
+++ database/migrations/2022_11_28_083020_create_cours_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cours', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('image_url');
+            $table->text('description');
+            $table->text('programme');
+            $table->integer('year');
+            $table->date('date_debut');
+            $table->date('date_fin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cours');
+    }
+}
